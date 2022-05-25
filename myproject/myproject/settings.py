3c3fa9dd46ea26b5998fe9654364b7fd1e64c586
yashas a bharadwaj <yashasbharadwaj9@gmail.com>
2022-05-25 18:41:47 +0530

    added models

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
 
@@ -132,3 +132,5 @@ STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
